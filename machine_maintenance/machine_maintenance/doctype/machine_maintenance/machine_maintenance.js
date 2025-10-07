bfbaf40defd6fbb16886594b22484459832180be
G-eethu <geethuk626@gmail.com>
2025-10-07 12:19:56 +0530

    feat:Client Scripts

--- machine_maintenance/machine_maintenance/doctype/machine_maintenance/machine_maintenance.js
+++ machine_maintenance/machine_maintenance/doctype/machine_maintenance/machine_maintenance.js
@@ -2,11 +2,40 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on("Machine Maintenance", {
-    refresh(frm) {
-        if (frm.doc.docstatus === 1) return;
-        show_notes(frm);
+    mark_completed(frm) {
+        frappe.confirm(
+            "Are you sure you want to mark this maintenance as completed?",
+            () => {
+                frm.set_value("status", "Completed");
+                frm.set_value("completion_date", frappe.datetime.get_today());
+                frm.save_or_update();
+                frappe.show_alert({
+                    message: __("Maintenance marked as Completed"),
+                    indicator: "green",
+                });
+            }
+        );
     },
+
+    refresh(frm) {
+        frm.toggle_display("notes_html", frm.doc.status !== "Scheduled");
+
+        if (frm.doc.maintenance_date < frappe.datetime.get_today() && frm.doc.status !== "Completed") {
+            frm.set_value("status", "Overdue");
+        }
+    },
+
+    onload(frm) {
+        if (frm.is_new()) {
+            frm.set_value("maintenance_date", frappe.datetime.get_today());
+        }
+    },
+
+    status(frm) {
+        frm.toggle_display("notes_html", frm.doc.status !== "Scheduled");
+    }
 });
+
 
 function show_notes(frm) {
     const crm_notes = new erpnext.utils.CRMNotes({
@@ -16,3 +45,28 @@ function show_notes(frm) {
     crm_notes.refresh();
 }
 
+frappe.ui.form.on("Parts Used", {
+    qty(frm, cdt, cdn) {
+        let row = frappe.get_doc(cdt, cdn);
+        row.amount = row.qty * row.rate;
+        frm.refresh_field("parts_used");
+        calculate_total(frm);
+    },
+    rate(frm, cdt, cdn) {
+        let row = frappe.get_doc(cdt, cdn);
+        row.amount = row.qty * row.rate;
+        frm.refresh_field("parts_used");
+        calculate_total(frm);
+    },
+    parts_used_remove(frm) {
+        calculate_total(frm);
+    }
+});
+
+function calculate_total(frm) {
+    let total = 0;
+    frm.doc.parts_used.forEach((d) => {
+        total += d.amount;
+    });
+    frm.set_value("cost", total);
+}
